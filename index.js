4310f248e0d5f8cc9e8d4ac6185aae9f69643074
catupperman <catupperman@gmail.com>
2021-08-11 19:54:58 -0500

    trying to get HTML generation to work

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const inquirer = require('inquirer');
 const path = require("path");
-const jest = require("jest");
+//const jest = require("jest");
 const fs = require("fs");
 const employee = require("./lib/Employee.js");
-const engineer = require("./lib/Engineer.js");
-const intern = require("./lib/Intern.js");
-const manager = require("./lib/Manager.js");
-const generate = require('./lib/htmlGenerate');
+const Intern = require("./lib/Intern.js");
+const render = require('./lib/htmlGenerate');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
 let team = [];
 
-const OUTPUT_DIR = path.resolve(__dirname, 'output');
-const outputDirection = path.join(OUTPUT_DIR, 'employee.html');
+//const OUTPUT_DIR = path.resolve(__dirname, 'output');
+//const outputDirection = path.join(OUTPUT_DIR, 'employee.html');
 
 function managerPrompt() {
     inquirer.prompt({
@@ -18,6 +18,12 @@ function managerPrompt() {
         message: "What is the Manager's Office Number?",
         name: "officeNumber"
     }).then((answers) => {
+        const name = answers.name
+        const id = answers.idnumber
+        const email = answers.email
+        const officeNumber = answers.officeNumber
+        const teamMember = new Manager(name, id, email, officeNumber)
+        team.push(teamMember)
         anotherUser(answers);
     })
 }
@@ -28,6 +34,12 @@ function engineerPrompt() {
         message: "What is the Engineer's GitHub Account Name?",
         name: "gitHub"
     }).then((answers) => {
+        const name = answers.name
+        const id = answers.idnumber
+        const email = answers.email
+        const gitHub = answers.gitHub
+        const teamMember = new Engineer(name, id, email, gitHub)
+        team.push(teamMember)
         anotherUser(answers);
     })
 }
@@ -38,6 +50,12 @@ function internPrompt() {
         message: "What school did/does the intern attend?",
         name: "school"
     }).then((answers) => {
+        const name = answers.name
+        const id = answers.idnumber
+        const email = answers.email
+        const school = answers.school
+        const teamMember = new Intern(name, id, email, school)
+        team.push(teamMember)
         anotherUser(answers);
     })
 
@@ -100,12 +118,30 @@ function mainMenu() {
     })
 }
 
+function generateHTML(){
+    const htmlArr = []
+    const employeeHTML = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Office Memebers</title>
+    </head>
+    <body>
+        <div class="heading-container">
+            <div class="row">
+                <h1 class="display-1 text-center"> The Office </h1>
+            </div>
+        </div>
+    </body>
+    </html>
+    `
+    }
+
+
 mainMenu();
 
-function generateHTML(){
-    fs.writeFile(outputDirection, generate(team), (err)=>{
-        err ? console.log(err): console.log('You made it');
-    })
-}
 //generate an HTML page pulling a function from each js in the library
 //another function to see if there is any more employees needed to be added
